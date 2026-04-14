54b2a4012d08066ee73cfbecb2d8cece0bebba72
m-montefalcon <mmontefalconschlprps@gmail.com>
2026-04-14 11:02:43 +0800

    Partial Feat: Feed Pages

--- app/(main)/feed/page.tsx
+++ app/(main)/feed/page.tsx
@@ -1,5 +1,21 @@
-import FeedPage from "@/app/components/feed/FeedPage";
+import { fetchPosts } from "@/app/actions/feed/actions";
 
-export default function Feed() {
-  return <FeedPage />;
+export default async function Page() {
+  const posts = await fetchPosts();
+  // TODO: Implement the feed UI to display the posts. For now, we just log them.
+  return (
+    <div>
+      {posts.map(
+        (
+          post: { id: string; title: string; content: string },
+          index: number,
+        ) => (
+          <div key={index}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ),
+      )}
+    </div>
+  );
 }
